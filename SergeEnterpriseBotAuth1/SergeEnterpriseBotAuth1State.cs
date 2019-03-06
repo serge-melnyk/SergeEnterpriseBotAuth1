2c96dd39eca836b1e7f43776d86342f4d968aeb0
Serhii V. Melnyk <dozarabotok@gmail.com>
2019-03-06 17:20:06 +0200

    Add project files.

---
+++ SergeEnterpriseBotAuth1/SergeEnterpriseBotAuth1State.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace SergeEnterpriseBotAuth1
+{
+    public class SergeEnterpriseBotAuth1State : DialogState
+    {
+    }
+}
